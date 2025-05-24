4199014c3498e3234b0c73e476f5cd12ce9f049c
ALAN JOY <alanjoy812006@gmail.com>
2025-05-24 10:47:41 +0530

    singly_linked_list.c
    The file was created and code has been written

---
+++ Linked_list/singly_linked_list.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//declaration of struct variable
+struct Node {
+  int data;
+  struct Node* next;
+}
+
+int main()
+{  int N, count;
+  struct Node* head = NULL;  
+  printf("Enter the number of nodes : \n");
+  scanf("%d",&N);
+  while(count != N)
+    {
+      int data;
+      printf("Enter the data \n");
+      scanf("%d",&data);
+      create_node(data);
+      count++;
+    }
+}
+
+struct Node* create_node(data)
+{
+  struct Node* node = (struct Node*) malloc(sizeof(struct Node));
+  node->data = data;
+  node->next = NULL;
+  return node;
+}
